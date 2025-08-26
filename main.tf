7eafdf6fdd60c769ad59e28c0820715f59179580
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-26 22:51:04 +0530

    Extra security group fixed

--- main.tf
+++ main.tf
@@ -61,8 +61,6 @@ module "blog_alb" {
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
-  security_group = [module.blog_sg.security_group_id]
-
 
   target_groups = [
     {
