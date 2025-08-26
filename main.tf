f949872c3d620d693514e5639d258168673b94ad
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-26 18:33:25 +0530

    rectified error to create security group

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ type = "ingress"
 from_port = 80
 to_port = 80
 protocol = "tcp"
-cidr_blocks = [0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -48,7 +48,7 @@ type = "ingress"
 from_port = 443
 to_port = 443
 protocol = "tcp"
-cidr_blocks = [0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
@@ -57,7 +57,7 @@ type = "egress"
 from_port = 0
 to_port = 0
 protocol = "-1"
-cidr_blocks = [0.0.0.0/0]
+cidr_blocks = ["0.0.0.0/0"]
 
 security_group_id = aws_security_group.blog.id
 }
