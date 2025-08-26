81d7c594c41bc50ba0246dc341546542eee0a7b9
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-26 22:10:40 +0530

    update alb error

--- main.tf
+++ main.tf
@@ -48,30 +48,38 @@ subnet_id = module.blog_vpc.public_subnets[0]
 
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
-
   name    = "blog-alb"
+  
+  load_balancer_type = "application"
+  
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
 
-  listeners = {
-    ex-http-https-redirect = {
+  target_groups = {
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+	  targets = {
+	  my_target = {
+      target_id        = aws_instance.blog.id
+	  port = 80
+    }
+  }
+  }
+  }
+  
+  http_tcp_listeners = [
+  {
       port     = 80
       protocol = "HTTP"
-      
+      target_groups_index = 0
     }
   }
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
 
   tags = {
     Environment = "Dev"
