57627a651600e849cba23b4e250bf3ccb5cecba7
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-26 22:46:29 +0530

    error fixed autoscaling

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
   max_size = 2
   
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_groups_arns = module.blog-alb.target_groups_arns
+  target_group_arns = module.blog_alb.target_groups_arns
   
   image_id    = data.aws_ami.app_ami.id
   instance_type = var.instance_type
