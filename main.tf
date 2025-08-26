a112412fd0a11a03fa1e9e5c3129f3a2f2f0de72
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-26 17:41:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "WEBSRVR"
   }
@@ -31,8 +33,35 @@ resource "aws_security_group" "blog" {
 name = "ASG_blog"
 description = "Allow http and https inbound. Allow everything outbound"
 
-vpc_id = da
+vpc_id = data.aws_vpc.default.id
 }
 
+resource "aws_security_group_rule" "blog_http_in" {
+type = "ingress"
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = [0.0.0.0/0]
 
+security_group_id = aws_security_group.blog.id
+}
 
+resource "aws_security_group_rule" "blog_https_in" {
+type = "ingress"
+from_port = 443
+to_port = 443
+protocol = "tcp"
+cidr_blocks = [0.0.0.0/0]
+
+security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_everything_out" {
+type = "egress"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = [0.0.0.0/0]
+
+security_group_id = aws_security_group.blog.id
+}
