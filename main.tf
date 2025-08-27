3299ffd587cd4aef3bb2f5d822b0b2190cadda06
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-27 11:32:40 +0530

    Autoscaling Done

--- main.tf
+++ main.tf
@@ -44,8 +44,9 @@ module "autoscaling" {
   
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id]
   
-  image_id    = data.aws_ami.app_ami.id
+  image_id   = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
 }
