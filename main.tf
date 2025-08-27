3b90984d50e9b13def0b415dc346c7a8d92ea7c6
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-27 15:10:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog_autoscaling" {
   min_size            = var.asg_min
   max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
