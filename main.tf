9599976e05f6e75bc843685ef37e97b27ed71016
pallabkundu1986 <pallabkundu.imps@gmail.com>
2025-08-27 12:39:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,12 +33,12 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+ 
 
   name = "blog"
 
-  min_size            = 1
-  max_size            = 2
+  min_size            = 0
+  max_size            = 1
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
